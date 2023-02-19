ca37f453e01fa1deb62593e1c9d7a7f54214c87e
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-02-19 13:38:23 -0800

    Updated the NavBar component based on the figma designs
    Created the initial elements to the NavBar

---
+++ dev/client/src/components/NavBar/NavBar.js
@@ -0,0 +1,24 @@
+import "./NavBar.css";
+import UserIcon from "../../images/userIcon.png"
+
+export default function NavBar(){
+    return (
+        <div className="nav-bar">
+            <div className="menu-logo">
+                <div className="menu-button">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <p className="brand-name">audiovision</p>
+            </div>
+            <div className="login">
+                <button className="login-button">
+                    <p className="login-text">Login</p>
+                    <img src={UserIcon} className="user-icon"/>
+                    {/* <a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by Freepik - Flaticon</a> */}
+                </button>
+            </div>
+        </div>
+    );
+}
