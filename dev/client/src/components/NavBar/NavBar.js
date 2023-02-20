fa00eadee98523a1b9a752bc89b37c8b83d0c9d5
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-02-20 11:07:05 -0800

    Updated NavBar.js
    added an onclick action that takes a function passed as an argument to the NavBar

--- dev/client/src/components/NavBar/NavBar.js
+++ dev/client/src/components/NavBar/NavBar.js
@@ -1,11 +1,11 @@
 import "./NavBar.css";
 import UserIcon from "../../images/userIcon.png"
 
-export default function NavBar(){
+export default function NavBar({showMenu}){
     return (
         <div className="nav-bar">
             <div className="menu-logo">
-                <div className="menu-button">
+                <div className="menu-button" onClick={showMenu}>
                     <div></div>
                     <div></div>
                     <div></div>
@@ -15,7 +15,7 @@ export default function NavBar(){
             <div className="login">
                 <button className="login-button">
                     <p className="login-text">Login</p>
-                    <img src={UserIcon} className="user-icon"/>
+                    <img src={UserIcon} className="user-icon" alt="user icon"/>
                     {/* <a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by Freepik - Flaticon</a> */}
                 </button>
             </div>
