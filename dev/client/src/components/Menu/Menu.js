116b1970650277a3161388bfa12a22e59b2a2092
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-02-20 11:04:07 -0800

    Created the initial structure of the menu

---
+++ dev/client/src/components/Menu/Menu.js
@@ -0,0 +1,12 @@
+import "./Menu.css"
+
+export default function Menu(){
+    return (
+        <div className="menu">
+            <p>hello</p>
+            <p>gerifbwrijf wfewf ewfew fewf ew</p>
+            <p>gerifbwrijf wfewf ewfew fewf ew</p>
+            <p>gerifbwrijf wfewf ewfew fewf ew</p>
+        </div>
+    )
+}
