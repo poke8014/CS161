0eaa9d1ea3aa180c4b80f5c632ae663b5deb72cf
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-02-20 11:10:05 -0800

    Updated UploadPage.js
    Added css to the file. Also, I created a React state to keep track of whether the menu was clicked or not.  Also, I added a parameter to the NavBar that takes the toggleShowMenu function which toggles the state of showMenu

--- dev/client/src/pages/UploadPage/UploadPage.js
+++ dev/client/src/pages/UploadPage/UploadPage.js
@@ -1,10 +1,23 @@
 import React from "react"
 import NavBar from "../../components/NavBar/NavBar";
+import Menu from "../../components/Menu/Menu";
+import "./UploadPage.css"
 
 export default function UploadPage() {
+    
+    const [showMenu, setShowMenu] = React.useState(false)
+    
+    function toggleShowMenu(){
+        console.log(showMenu)
+        setShowMenu(prevState => !prevState)
+    }
+
     return (
         <div className="upload-page">
-            <NavBar />
+            <NavBar 
+                showMenu={toggleShowMenu} 
+            />
+            {showMenu && <Menu/>}
         </div>
     );
 }
