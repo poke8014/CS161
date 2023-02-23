f11856a6cdd06ff744a7057b163218ab45e7ea9b
poke8014 <theluc04@yahoo.com>
2023-02-22 20:32:38 -0800

    base CRUD operations and testing completed

--- dev/server/routes/users.js
+++ dev/server/routes/users.js
@@ -1,25 +1,77 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user')
 
 // Getting all
-router.get('/', (req, res) => {
-    res.send('Hello World')
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
+
 // Getting one
-router.get('/:id', (req, res) => {
-    req.params.id
+router.get('/:id', getUser, (req, res) => {
+    res.json(res.user);
 });
+
 // Creating one
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password,
+        // songLibrary: {}
+    });
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 });
-// Updating one
-router.patch('/', (req, res) => {
 
+// Updating one
+router.patch('/:id', getUser, async (req, res) => {
+    if (req.body.email != null) {
+        res.user.email = req.body.email;
+    }
+    if (req.body.password != null) {
+        res.user.password = req.body.password;
+    }
+    try {
+        const updatedUser = await res.user.save();
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
 });
 // Deleting one
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', getUser, async (req, res) => {
+    let id = res.user.id
+    try {
+        await res.user.remove();
+        res.json({ message: `Deleted user ${id}` })
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
+
+// Middleware
+async function getUser(req, res, next) {
+    let user
+    try {
+        user = await User.findById(req.params.id)
+        if (user == null) {
+            return res.status(404).json({ message: 'Cannot find user' })
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+
+    res.user = user;
+    next();
+};
 
 module.exports = router
