0718b85ab6d3059f7c0c03e6926e77b32f67ffb3
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-02-27 14:02:22 -0800

    Update LoginPage.js

--- dev/src/pages/loginPage/LoginPage.js
+++ dev/src/pages/loginPage/LoginPage.js
@@ -10,7 +10,17 @@ export default function LoginPage(){
             />
             <div className="login-signup-container">
                 <div className="login-signup-box">
-                    
+                    <h1>Login</h1>
+                    <div className="login-signup-buttons">
+                        <button className="login-button">Login</button>
+                        <button className="signup-button">Sign Up</button>
+                    </div>
+                    <form>
+                        <input type={"text"} placeholder="Email Address" />
+                        <input type={"text"} placeholder="Password" />
+                        <p>Forgot Password?</p>
+                        <input type={"submit"}  value="Login"/>
+                    </form>
                 </div>
             </div>
         </div>
