277277d487a5991d88afa9caae9cb47a9adb3a11
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-02-27 16:55:08 -0800

    Created initial state to handle the change of forms

--- dev/src/pages/loginPage/LoginPage.js
+++ dev/src/pages/loginPage/LoginPage.js
@@ -1,7 +1,15 @@
 import NavBar from "../../components/NavBar/NavBar"
+import React from "react";
 import "./LoginPage.css"
 
 export default function LoginPage(){
+
+    const [formType, setFormType] = React.useState("login");
+
+    function changeFormType(e){
+        e.preventDefault()
+    }
+
     return (
         <div className="login-page">
             <NavBar 
@@ -12,13 +20,13 @@ export default function LoginPage(){
                 <form className="login-signup-box" method="get">
                     <p className="form-type">Login</p>
                     <div className="login-signup-buttons">
-                        <button className="login-button-form">Login</button>
-                        <button className="signup-button-form">Sign Up</button>
+                        <button className="login-button-form" onClick={changeFormType}>Login</button>
+                        <button className="signup-button-form" onClick={changeFormType}>Sign Up</button>
                     </div>
                     <input type={"text"} placeholder="Email Address" />
                     <input type={"text"} placeholder="Password" />
-                    <p>Forgot Password?</p>
-                    <input type={"submit"}  value="Login"/>
+                    <p className="forgot-password">Forgot Password?</p>
+                    <input className="login-button-submit" type={"submit"} value="Login"/>
                     <p>Don't have an account? Sign Up now</p>
                 </form>
             </div>
