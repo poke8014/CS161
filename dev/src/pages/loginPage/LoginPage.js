b4c9373096aa49c47452aa76e5f50a0ce380038b
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-02-28 21:04:32 -0800

    updated login page

--- dev/src/pages/loginPage/LoginPage.js
+++ dev/src/pages/loginPage/LoginPage.js
@@ -23,11 +23,13 @@ export default function LoginPage(){
                         <button className="login-button-form" onClick={changeFormType}>Login</button>
                         <button className="signup-button-form" onClick={changeFormType}>Sign Up</button>
                     </div>
-                    <input type={"text"} placeholder="Email Address" />
-                    <input type={"text"} placeholder="Password" />
+                    <input type={"email"} placeholder="Email Address" />
+                    <input type={"password"} placeholder="Password" />
                     <p className="forgot-password">Forgot Password?</p>
                     <input className="login-button-submit" type={"submit"} value="Login"/>
-                    <p>Don't have an account? Sign Up now</p>
+                    <p className="no-account">Don't have an account? 
+                        <button className="signup-now-button" onClick={changeFormType}>Sign Up now</button>
+                    </p>
                 </form>
             </div>
         </div>
