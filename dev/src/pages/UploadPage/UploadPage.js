0d331d7d77aa898200dfb03d33dad73ea9a0b100
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-03-08 18:46:56 -0800

    updated the menu

--- dev/src/pages/UploadPage/UploadPage.js
+++ dev/src/pages/UploadPage/UploadPage.js
@@ -8,7 +8,15 @@ import "./UploadPage.css"
 export default function UploadPage() {
     
     const [showMenu, setShowMenu] = React.useState(false)
+    // const [users, setUsers] = React.useState();
     const navigate = useNavigate();
+
+    // React.useEffect(() => {
+    //     fetch("/users")
+    //     .then(response => response.json())
+    //     .then(data => console.log(data))
+    //     .catch(err => console.log("error: " + err))
+    // }, []);
     
     function toggleShowMenu(){
         setShowMenu(prevState => !prevState)
@@ -18,13 +26,26 @@ export default function UploadPage() {
         navigate("/visualization")
     }
 
+    const menuItems = [
+        "Click on an existing audio",
+        "Audio 1",
+        "Audio 2",
+        "Audio 3",
+        "Audio 4",
+        "Audio 5",
+        "Audio 6"
+    ];
+
     return (
         <div className="upload-page">
             <NavBar 
                 openMenu={toggleShowMenu}
             />
             <div className="content">
-                {showMenu && <Menu/>}
+                {showMenu && 
+                <Menu
+                    menuItems={menuItems}        
+                />}
                 <div className="upload-box">
                     <div className="upload-options">
                         <button className="upload-audio-button" onClick={handleAudioUpload}>
