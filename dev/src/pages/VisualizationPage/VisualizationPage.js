705f384252d6af02d3d89be9dd784c4e9d71a394
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-03-08 18:46:36 -0800

    Initial visualization page setup

--- dev/src/pages/VisualizationPage/VisualizationPage.js
+++ dev/src/pages/VisualizationPage/VisualizationPage.js
@@ -1,12 +1,30 @@
 import "./VisualizationPage.css"
 import NavBar from "../../components/NavBar/NavBar"
+import Menu from "../../components/Menu/Menu"
+import React from "react"
 // import { useNavigate } from "react-router-dom";
 
 export default function VisualizationPage(){
+
+    const [showMenu, setShowMenu] = React.useState(false)
+
+    function toggleShowMenu(){
+        setShowMenu(prev => !prev)
+    }
+
     return (
         <div className="visualization-page">
-             <NavBar/>
-             <p>Work in Progress.....</p>
+             <NavBar
+                openMenu={toggleShowMenu}
+             />
+             <main className="content-container">
+                {showMenu &&
+                    <Menu/>
+                }
+                <div className="visualization-container">
+                    <p>Work in Progress.....</p>
+                </div>
+             </main>
         </div>
     )
 }
