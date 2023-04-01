835386d0e37e6b221c621630fd5fa0968060f3fa
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-03-31 17:58:05 -0700

    Update mongoDBController.js
    Adding logic to use access and refresh tokens

--- dev/server/controllers/mongoDBController.js
+++ dev/server/controllers/mongoDBController.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 // Creating one
 async function createUser(req, res) {
@@ -27,21 +29,37 @@ async function createUser(req, res) {
 async function handleLogin(req, res) {
     const { email, password } = req.body;
     if (!email || !password) return res.status(400).json({'message': 'Email and password are required.'});
-    try {
-        const foundUser = await User.findOne({ email: email }); 
+    const foundUser = await User.findOne({ email: email }); 
 
-        if (!foundUser) return res.status(401).json({ message: 'Invalid email or password.' });
-    
-        //evaluate hashed password
-        const match = await bcrypt.compare(password, foundUser.password);
-        if (match){
-            res.json({ success: `User is logged in!` });
-        }else{
-            res.status(401).json({ message: 'Invalid email or password.' });
-        }   
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+    if (!foundUser) return res.sendStatus(401);
+
+    //evaluate hashed password
+    const match = await bcrypt.compare(password, foundUser.password);
+    if (match){
+        // JWTs
+        const accessToken = jwt.sign(
+            {"email": foundUser.email},
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '300s' }
+        )
+        const refreshToken = jwt.sign(
+            {"email": foundUser.email},
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        )
+        // Saving refreshToken with current user
+        const query = { _id: foundUser._id };
+        const update = { refreshToken };
+        const options = { new: true };
+
+        const updatedUser = await User.findOneAndUpdate(query, update, options);
+        
+        res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24*60*60*1000 });
+        res.json({ accessToken });
+        res.json({ success: 'User is logged in!' });
+    }else{
+        res.sendStatus(401)
+    }   
 }
 
 // Updating one
