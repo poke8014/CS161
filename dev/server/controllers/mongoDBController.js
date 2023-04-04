6aecd1a8c006d17c4e8e58a5aebb6b78bb619112
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-04-04 01:14:22 -0700

    Added logout functionality
    Added logout functionality in the server side. It clears the cookie and the filed in the database

--- dev/server/controllers/mongoDBController.js
+++ dev/server/controllers/mongoDBController.js
@@ -58,6 +58,25 @@ async function handleLogin(req, res) {
     }   
 }
 
+async function handleLogout(req, res) {
+    const cookies = req.cookies
+    if (!cookies?.jwt) return res.sendStatus(204) // success, no content
+    const refreshToken = cookies.jwt
+
+    const foundUser = await User.findOne({ refreshToken }); 
+
+    if (!foundUser) {
+        res.clearCookie('jwt', {httpOnly:true}, {maxAge:14*60*60*1000})
+        return res.sendStatus(204)
+    }
+
+    //delete it from db
+    const deleteToken = await User.findOneAndUpdate({email: foundUser.email}, {$unset: {refreshToken}});
+
+    res.clearCookie('jwt', {httpOnly:true}, {maxAge:14*60*60*1000})
+    res.sendStatus(204)
+}
+
 // Updating one
 async function updateUser(req, res) {
     if (req.body.email != null) {
@@ -101,4 +120,5 @@ async function getUser(req, res, next) {
     next();
 };
 
-module.exports = { getUser, createUser, deleteUser, updateUser, handleLogin };
+module.exports = { getUser, createUser, deleteUser, 
+            updateUser, handleLogin, handleLogout };
