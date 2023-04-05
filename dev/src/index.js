15c03230e6c45025bf629a01e2be8b9dcc4e8bc4
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-04-05 00:52:56 -0700

    Updating routing
    I updated the routing in the App.js and index.js so that we can use protected routing when the user wants to download the visualization

--- dev/src/index.js
+++ dev/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './context/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,7 +9,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
