1ebdf9045b40b2975624f8dadd4b29da949eb210
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-04-19 19:08:48 -0700

    Added state to file context to know what file has been selected

--- dev/src/components/FileContext.js
+++ dev/src/components/FileContext.js
@@ -4,9 +4,10 @@ export const FileContext = createContext();
 
 export function FileProvider({ children }) {
   const [fileData, setFileData] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null)
 
   return (
-    <FileContext.Provider value={{ fileData, setFileData }}>
+    <FileContext.Provider value={{ fileData, setFileData, selectedFile, setSelectedFile}}>
       {children}
     </FileContext.Provider>
   );
