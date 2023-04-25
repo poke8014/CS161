0e7fe26c6aefe87bc1c952077cbea54c4ad996d6
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-04-25 01:02:35 -0700

    Added navigation to the upload page and added audio-visualizer
    Added routing and elements to handle animations

--- dev/src/pages/HomePage/HomePage.js
+++ dev/src/pages/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useNavigate } from "react-router-dom";
 import "./HomePage.css"
 import catChill from "../../images/music-g0ceccf190_1280.png"
 import headMusic from "../../images/had-white.png"
@@ -8,75 +9,101 @@ import NavBar from "../../components/NavBar/NavBar"
 
 export default function HomePage(){
 
-    React.useEffect(() => {
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => {
-              if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                entry.target.style.animationPlayState = "running";
-              }
-            });
-          }, {
-            threshold: 0.5
-          });
-      
-          const element = document.querySelector('.description');
-          const element2 = document.querySelector('.second img');
+  const navigate = useNavigate()
 
-          observer.observe(element2);
-          observer.observe(element);
-      
-          // cleanup function
-          return () => {
-            observer.disconnect();
+  const descriptionText = React.useRef(null)
+  const musicImage = React.useRef(null)
+  const dataAnalysisDesRef = React.useRef(null)
+
+  const audioVisualizerRef = React.useRef(null)
+
+  const handleMouseEnter = () => {
+    audioVisualizerRef.current.style.visibility = "visible";
+  };
+
+  const handleMouseLeave = () => {
+    audioVisualizerRef.current.style.visibility = "hidden";
+  };
+
+  function handleClick(){
+    navigate("/upload")
+  }
+
+  React.useEffect(() => {
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            entry.target.style.animationPlayState = "running";
           }
-    },[])
+        });
+      }, {
+        threshold: 0.5
+      });
 
-    return (
-        <main className="home-page">
-            <NavBar showMenuButton={false}/>
-            <div className="first">
-                <div className="left">
-                    <p>Unleash the power <span>of music with</span><span className="audiovision-span">audiovision</span></p>
-                    <button className="start">Let's Go!</button>
-                </div>
-                <img src={catChill} />
-            </div>
-            <p className="description">
-                audiovision is an interactive web app that brings 
-                your audio to life with stunning visuals. Upload your own audio files or 
-                browse our collection, and experience a whole new way of visualizing sound.
-            </p>
-            <div className="second">
-                <img src={headMusic} />
-                <p>Let audiovision reveal the hidden emotions in your favorite music</p>
-            </div>
-            <p className="data-analysis-des">
-                audiovision uses audio data analysis to create stunning 
-                visualizations that bring your audio to life. 
-            </p>
-            <img src={audioVid} />
-            {/* <video className="visual-vid" src={audioVid} autoPlay={true} /> */}
-            <button className="last">Let's Go!</button>
-            <br />
-            {/* Animation when hovering over the audiovision */}
-            <div className="audio-visualizer">
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-            </div>
-        </main>
-    )
+      observer.observe(descriptionText.current)
+      observer.observe(musicImage.current)
+      observer.observe(dataAnalysisDesRef.current)
+  
+      // cleanup function
+      return () => {
+        observer.disconnect();
+      }
+  },[])
+
+  return (
+      <main className="home-page">
+          <NavBar showMenuButton={false}/>
+          <div className="first">
+              <div className="left">
+                  <p>Unleash the power 
+                    <span>of music with</span>
+                    <span className="audiovision-span" onMouseEnter={handleMouseEnter} 
+                          onMouseLeave={handleMouseLeave}>audiovision</span>
+                  </p>
+                  <button className="start" onClick={handleClick}>Let's Go!</button>
+              </div>
+              <img src={catChill} />
+          </div>
+          <p className="description" ref={descriptionText}>
+              <span className="audio-visual" onMouseEnter={handleMouseEnter} 
+              onMouseLeave={handleMouseLeave}>audiovision</span> is an interactive web app that brings 
+              your audio to life with stunning visuals. Upload your own audio files or 
+              browse our collection, and experience a whole new way of visualizing sound.
+          </p>
+          <div className="second">
+              <img src={headMusic} ref={musicImage}/>
+              <p>Let <span className="audio-visual" onMouseEnter={handleMouseEnter} 
+              onMouseLeave={handleMouseLeave}>audiovision</span> reveal the hidden emotions in your favorite music</p>
+          </div>
+          <p className="data-analysis-des" ref={dataAnalysisDesRef} >
+            <span className="audio-visual" onMouseEnter={handleMouseEnter} 
+              onMouseLeave={handleMouseLeave}>audiovision</span> uses audio data analysis to create stunning 
+              visualizations that bring your audio to life. 
+          </p>
+          <img src={audioVid} className="visual-vid" />
+          {/* <video className="visual-vid" src={audioVid} autoPlay={true} /> */}
+          <button className="last" onClick={handleClick}>Let's Go!</button>
+          <br />
+          {/* Animation when hovering over the audiovision */}
+          <div className="audio-visualizer" ref={audioVisualizerRef}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
+      </main>
+  )
 }
