18b402e4ff11fd43b3b991d586a9aca017330ed2
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-04-25 17:43:13 -0700

    Fixed bug
    if canvas is clicked again, it will not overlap

--- dev/src/sketches/TestSketchM.js
+++ dev/src/sketches/TestSketchM.js
@@ -3,6 +3,7 @@ import React, { useRef, useEffect } from 'react';
 function TestSketchM(props) {
   const { audioLink } = props;
   const canvasRef = useRef(null);
+  const audioElementRef = useRef(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -33,47 +34,47 @@ function TestSketchM(props) {
       }
     }
 
-    const handleClick = async () => {
-      try {
-        // create audio context
-        const AudioContext = window.AudioContext || window.webkitAudioContext;
-        const audioCtx = new AudioContext();
-      
-        // create analyser node
-        const analyser = audioCtx.createAnalyser();
-        analyser.fftSize = 256;
-      
-        // connect analyser to audio source
-        const audioElement = new Audio(audioLink);
-        audioElement.crossOrigin = 'anonymous';
-        const sourceNode = audioCtx.createMediaElementSource(audioElement);
-        sourceNode.connect(analyser);
-        analyser.connect(audioCtx.destination);
-      
-        // resume AudioContext if necessary
-        if (audioCtx.state === 'suspended') {
-          await audioCtx.resume();
-        }
-      
-        // start audio playback
-        await audioElement.play();
-      
-        // create data array for visualizations
-        const bufferLength = analyser.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
-      
-        draw(analyser, dataArray);
-      } catch (err) {
-        console.error(err);
+    const handleClick = () => {
+      // create audio context
+      const AudioContext = window.AudioContext || window.webkitAudioContext;
+      const audioCtx = new AudioContext();
+    
+      // create analyser node
+      const analyser = audioCtx.createAnalyser();
+      analyser.fftSize = 256;
+    
+      // connect analyser to audio source
+      const audioElement = new Audio(audioLink);
+      audioElement.crossOrigin = 'anonymous';
+      audioElementRef.current = audioElement; // save audio element to a ref
+      const sourceNode = audioCtx.createMediaElementSource(audioElement);
+      sourceNode.connect(analyser);
+      analyser.connect(audioCtx.destination);
+    
+      // resume AudioContext if necessary
+      if (audioCtx.state === 'suspended') {
+        audioCtx.resume();
       }
+    
+      // start audio playback
+      audioElement.play();
+    
+      // remove canvas click event listener to avoid overlapping audio playback
+      canvas.removeEventListener('click', handleClick);
+    
+      // create data array for visualizations
+      const bufferLength = analyser.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
+    
+      draw(analyser, dataArray);
     };
 
+    // add click event listener to the canvas
     canvas.addEventListener('click', handleClick);
 
     return () => {
       // cleanup function
       canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-      canvas.removeEventListener('click', handleClick);
     };
   }, [audioLink]);
 
