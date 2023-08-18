a3cb98424e4d1f8eaf74f643ae4aa270f6f63d0c
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-08-18 03:44:30 -0700

    moved the play button to the visualization page

--- dev/src/pages/VisualizationPage/VisualizationPage.js
+++ dev/src/pages/VisualizationPage/VisualizationPage.js
@@ -4,9 +4,12 @@ import NavBar from "../../components/NavBar/NavBar"
 import SketchMenu from "../../components/SketchMenu/SketchMenu"
 import SketchSimpleBars from "../../sketches/SketchSimpleBars"
 import { FileContext } from "../../context/FileContext"
+import { VisualContext } from "../../context/VisualContext"
 import "./VisualizationPage.css"
 
 export default function VisualizationPage(){
+
+    const { playRef, audioPlaying } = React.useContext(VisualContext)
 
     const [showMenu, setShowMenu] = React.useState(false);
     const [defaultAudio, setDefaultAudio] = React.useState({});
@@ -68,8 +71,8 @@ export default function VisualizationPage(){
     return (
         <div className="visualization-page">
             <NavBar openMenu={toggleShowMenu}/>
-            <main className="content-container">
-                {showMenu && <SketchMenu />}
+            {showMenu && <SketchMenu />}
+            <div className="content-container">
                 <div className="visualization-container" ref={visualizationContainerSizeRef}>
                     {audioReady && <SketchSimpleBars 
                         audioLink={audioLink} 
@@ -77,7 +80,8 @@ export default function VisualizationPage(){
                         height={canvasHeight}
                     />}
                 </div>
-            </main>
+                <button ref={playRef} className={`play-button ${audioPlaying ? "" : "play"}`}>Play/Pause</button>
+            </div>
         </div>
     );
 }
