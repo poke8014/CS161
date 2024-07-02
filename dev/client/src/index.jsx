a72a8aad457d5109d1fd97777a6714b9413c40d3
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-07-02 16:34:03 -0700

    Replaced HashRouter for BrowserRouter
    We use BrowserRouter when we need to do a GET request to be sent to the server. In comparison, HashRouter is useful when you have a static file server and cannot configure the server to handle dynamic routes. In our case, we need to use BrowserRouter to know if the user is signed in. This fixes the bug of the page being stuck in the loading screen.

--- dev/client/src/index.jsx
+++ dev/client/src/index.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
+import { StrictMode } from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
-    <AuthProvider>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </AuthProvider>
-  </HashRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 );
